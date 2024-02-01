764eb0d4a3e9164ea794f02ae8bfd811dd054c3c
Ashu <ashuprasad29516@gmail.com>
2024-02-01 12:51:43 -0600

    feat: added structure for questions

--- src/github.com/prasadashu/QuizApp/Main.go
+++ src/github.com/prasadashu/QuizApp/Main.go
@@ -2,23 +2,55 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
 	"time"
 )
 
+type problems struct {
+	question string
+	answer   string
+}
+
+func parseProblems(records [][]string) []problems {
+	problemList := make([]problems, len(records))
+
+	for index, line := range records {
+		problemList[index] = problems{
+			question: line[0],
+			answer:   line[1],
+		}
+	}
+
+	return problemList
+}
+
 func main() {
+	// Get config variables
+	quizFile := flag.String("f", "quiz.csv", "path to quiz file")
+	flag.Parse()
+
 	// Open CSV file
-	file, err := os.Open("quiz.csv")
+	file, err := os.Open(*quizFile)
 
 	// Check if there is any error opening CSV file
 	if err != nil {
 		fmt.Println("Error while opening CSV file: ", err)
+		os.Exit(1)
 	}
 
 	// Read contents of CSV file
 	reader := csv.NewReader(file)
-	records, _ := reader.ReadAll()
+	records, err := reader.ReadAll()
+
+	// Check if there is any error while parsing CSV file
+	if err != nil {
+		fmt.Println("Error while parsing CSV file: ", err)
+	}
+
+	// Get problem list
+	problemList := parseProblems(records)
 
 	// Count of right answers
 	count := 0
@@ -33,15 +65,15 @@ func main() {
 		close(quizChannel)
 	}()
 
-	for i := 0; i < len(records); i++ {
+	for i := 0; i < len(problemList); i++ {
 		select {
 		case <-quizChannel:
-			fmt.Printf("Total score is %d out of %d", count, len(records))
+			fmt.Printf("Total score is %d out of %d", count, len(problemList))
 			return
 		default:
-			fmt.Print(records[i][0] + " = ")
+			fmt.Print(problemList[i].question + " = ")
 			fmt.Scan(&userAnswer)
-			if userAnswer == records[i][1] {
+			if userAnswer == problemList[i].answer {
 				count++
 			}
 		}
